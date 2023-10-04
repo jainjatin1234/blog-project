71eafde14a0d1c0de4cd6c62a8acd2246de259fd
jainjatin1234 <jainjatin748@gmail.com>
2023-10-05 01:25:32 +0530

    h

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -3,7 +3,7 @@ const local_url = "mongodb://127.0.0.1:27017/blogproject";
 const live_url ="mongodb+srv://jainjatin748:jatin123@cluster0.ksk727f.mongodb.net/blogproject?retryWrites=true&w=majority"
 
 const connectdb = () => {
-  return mongoose.connect(local_url)
+  return mongoose.connect(live_url)
 
   .then(()=>{
     console.log("Database connected....")
